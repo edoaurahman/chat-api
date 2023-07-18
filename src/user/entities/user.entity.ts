4f114fb05bd2c04592e07676e7caf324a2f20e28
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2023-07-18 13:58:47 +0700

    fix user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -11,7 +11,7 @@ export class User {
   @Column('varchar', { name: 'email', length: 50, unique: true })
   email: string;
 
-  @Column('text', { name: 'verification_code', unique: true })
+  @Column('varchar', { length: 6, name: 'verification_code', unique: true })
   verificationCode: string;
 
   @Column('datetime', { name: 'verified_at', nullable: true })
