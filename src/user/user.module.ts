3eb267b29a06b0dfe89c7ccb869aa3a9b7a7a1dd
Ridho Aulia' Rahman <edoaurahman@gmail.com>
2023-09-24 11:14:05 +0700

    fix mailer

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,6 +4,7 @@ import { UserController } from './user.controller';
 import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MulterModule } from '@nestjs/platform-express';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { MulterModule } from '@nestjs/platform-express';
     MulterModule.register({
       dest: './uploads/avatars',
     }),
+    HttpModule,
   ],
   controllers: [UserController],
   providers: [UserService],
